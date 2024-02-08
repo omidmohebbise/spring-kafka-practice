57bac41d2786e9b701754fa8c1b65a951d0327ab
omid mohebbi <omidmohebbise@gmail.com>
2024-02-08 16:37:52 +0100

    Implement simple producer / consumer

---
+++ spring-3-kafka/src/main/java/com/omidmohebbise/springkafka/example1/config/SimpleKafkaProducer.java
@@ -0,0 +1,24 @@
+package com.omidmohebbise.springkafka.example1.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class SimpleKafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(KafkaConfiguration.EXAMPLE1_KAFKA_TOPIC, message);
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void produce() {
+        sendMessage("Hello Kafka " + LocalDateTime.now());
+    }
+}
